998d47d9c28c411a2f2e8f405d750574bd6614d4
NikosMouzakitis <mzktsn@gmail.com>
2017-10-29 12:50:26 -0400

    Create system_example.c

---
+++ system_example.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <malloc.h>
+#include <sys/types.h>
+
+int main(int argc,char *argv[],char *envp[])
+{
+
+	system("cat main.c > file1");
+
+
+	return 0;
+}
